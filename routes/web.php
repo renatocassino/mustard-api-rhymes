b44089bd50968ad156e0fdee7a3273e39ee951cd
Renato Cassino <renatocassino@gmail.com>
2019-10-04 01:21:28 -0300

    Endpoint to get words to rhyme

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,28 @@
 |
 */
 
-$router->get('/', function () use ($router) {
+$router->get('/healthcheck', function () use ($router) {
     return $router->app->version();
+});
+
+$router->get('/api/v1/words/{language}/{word}', function($language, $word) use ($router) {
+    if ($language !== 'pt-br' && $language !== 'en') {
+        return response()->json(['error' => 'Must have language pt-br or en'], 404, []);
+    }
+
+    $allWords = explode("\n", file_get_contents("../database/seeds/words/$language.txt"));
+    $size = isset($_GET['size'])
+        ? (int) $_GET['size']
+        : 3;
+
+    $wordToSearch = substr($word, $size * -1);
+
+    return [
+        'data' => [
+            'language' => $language,
+            'words' => array_values(array_filter($allWords, function($w) use ($wordToSearch) {
+                return preg_match("/$wordToSearch$/", $w);
+            })),
+        ],
+    ];
 });
