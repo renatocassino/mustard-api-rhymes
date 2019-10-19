07971d6cb9a896fa54738ef7c6b6fe843221b975
Renato Cassino <renatocassino@gmail.com>
2019-10-19 20:27:15 -0300

    Route to view some lyric

--- src/Controller/LyricsController.php
+++ src/Controller/LyricsController.php
@@ -30,12 +30,43 @@ class LyricsController extends AbstractController implements AuthenticatedContro
     return $this->json([
       'data' => array_map(function($lyric) {
         return [
+          'id' => $lyric->getId(),
           'title' => $lyric->getTitle(),
           'lyric' => $lyric->getLyric(),
           'createdAt' => $lyric->getCreatedAt(),
           'updatedAt' => $lyric->getUpdatedAt(),
         ];
       }, $lyrics)
+    ]);
+  }
+
+  /**
+   * @Route("/api/v1/lyrics/{id}")
+   */
+  public function viewLyric(Request $request, $id) {
+    $user = $this->getUserByToken($request);
+
+    $lyric = $this->getDoctrine()
+      ->getRepository(Lyric::class)
+      ->findOneBy([
+        'user' => $user->id,
+        'id' => $id,
+      ]);
+
+    if (!$lyric) {
+      return $this->json([
+        'error' => 'Cannot find this lyric :/'
+      ], 404);
+    }
+
+    return $this->json([
+      'data' => [
+        'id' => $lyric->getId(),
+        'title' => $lyric->getTitle(),
+        'lyric' => $lyric->getLyric(),
+        'createdAt' => $lyric->getCreatedAt(),
+        'updatedAt' => $lyric->getUpdatedAt(),
+      ]
     ]);
   }
 }
