5ad97e86888b85211531537b512e0231e5640e4e
Renato Cassino <renatocassino@gmail.com>
2019-10-19 18:19:08 -0300

    Fix migrations and Entity

--- src/Migrations/Version20191016013217.php
+++ src/Migrations/Version20191016013217.php
@@ -22,8 +22,8 @@ final class Version20191016013217 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SEQUENCE lyric_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE lyric (id INT NOT NULL, title VARCHAR(255) NOT NULL, lyric TEXT NOT NULL, user_id INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE SEQUENCE lyrics_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE lyrics (id INT NOT NULL, title VARCHAR(255) NOT NULL, lyric TEXT NOT NULL, user_id INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema) : void
@@ -32,7 +32,7 @@ final class Version20191016013217 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE lyric_id_seq CASCADE');
-        $this->addSql('DROP TABLE lyric');
+        $this->addSql('DROP SEQUENCE lyrics_id_seq CASCADE');
+        $this->addSql('DROP TABLE lyrics');
     }
 }
--- src/Migrations/Version20191019210139.php
+++ src/Migrations/Version20191019210139.php
@@ -21,8 +21,8 @@ final class Version20191019210139 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, google_id VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, email_verified BOOLEAN NOT NULL, name VARCHAR(255) NOT NULL, given_name VARCHAR(255) NOT NULL, family_name VARCHAR(255) NOT NULL, picture VARCHAR(255) NOT NULL, locale VARCHAR(20) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE "users" (id INT NOT NULL, google_id VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, email_verified BOOLEAN NOT NULL, name VARCHAR(255) NOT NULL, given_name VARCHAR(255) NOT NULL, family_name VARCHAR(255) NOT NULL, picture VARCHAR(255) NOT NULL, locale VARCHAR(20) NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema) : void
@@ -31,9 +31,9 @@ final class Version20191019210139 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE users_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE users (id INT NOT NULL, google_id VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, email_verified BOOLEAN NOT NULL, name VARCHAR(255) NOT NULL, given_name VARCHAR(255) NOT NULL, family_name VARCHAR(255) NOT NULL, picture VARCHAR(255) NOT NULL, locale VARCHAR(10) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE "users"');
     }
 }
